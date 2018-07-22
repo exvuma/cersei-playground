3754c1191a94ad6145210287da7f9d945c816160
Victoria <bernard.victoria22@gmail.com>
2018-07-22 14:01:14 -0500

    Make GPIO work locally and on the RPI

---
+++ myGPIO.py
@@ -0,0 +1,9 @@
+# This is just a placeholder class for when the pi is being
+#  ran locally on a machine that is not the pi
+class GPIO:
+    def setup():
+        return "setting up"
+    def output(y, mode, place):
+        return "asda"
+    HIGH = "high"
+    LOW =  "LOW"
